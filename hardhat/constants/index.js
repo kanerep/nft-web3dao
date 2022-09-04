57f71b6b9a268fc3311a7140cc5c03b97ccfa4eb
kanerep <31217506+kanerep@users.noreply.github.com>
2022-09-04 09:34:46 +0100

    Learn Web 3 - CryptoDevs NFT

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0xF8cD8632D666e94cf2D0159Ec623fB604242B3C8';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
